ee94f621dc3aa0cf106bc07eea9dd0bac0b4e8e5
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2023-09-27 10:47:55 -0300

    Add files via upload

---
+++ maior que 10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+int valor1;
+
+
+
+printf ("digite o valor ");
+scanf("%d", &valor1 );            
+
+if (valor1 > 10)
+{
+    printf( " valor maior que 10");
+}
+else
+{
+    printf("valor menor que 10 ");
+}
+
+return 0;
+
+
+
+
+
+
+
+
+}
+
+
