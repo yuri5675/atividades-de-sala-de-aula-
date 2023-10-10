771e7f084d7214fca8672a75a90a6ee524879c8c
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2023-10-10 11:31:12 -0300

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int codigo;
+
+printf(" escolha uma opcao ");
+scanf("%d", codigo);
+
+switch (codigo){
+
+case 1:
+printf("picanha   R$: 25,00")
+break;
+
+case 2:
+printf("")
+} 
+}
