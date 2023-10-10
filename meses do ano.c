00cb282d222897173001215df64b741975908915
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2023-10-10 11:11:45 -0300

    Add files via upload

---
+++ meses do ano.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int mes; 
+
+printf ("digite o numero do mes: ");
+scanf ("%d", &mes ); 
+
+
+    switch (mes){
+
+        case 1: 
+printf("janeiro");
+break;
+
+case 2:
+printf(" fevereiro");
+break;
+
+case 3:
+printf(" março");
+break;
+
+case 4:
+printf (" abril");
+break;
+
+case 5:
+printf ("maio");
+break;
+
+case 6:
+printf ("junho ");
+break;
+
+case 7: 
+printf (" julho");
+break;
+
+case 8:
+printf ("agosto");
+break;
+
+case 9: 
+printf (" setembro ");
+break;
+
+case 10: 
+printf ("outubro");
+break;
+
+case 11:
+printf (" novembro ");
+break;
+
+case 12: 
+printf (" dezembro ");
+break;
+
+default:
+printf ("  invalida ");
+
+
+return 0;
+    }
+
+
+}
