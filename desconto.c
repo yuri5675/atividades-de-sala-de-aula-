d368a72982e30a541e780156cd3696ad3dce3468
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2023-10-11 11:18:51 -0300

    Add files via upload

---
+++ desconto.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include <ctype.h>
+
+
+int main(){
+
+printf("digite o valor do produro?:");
+scanf("%f", &precoproduto)
+
+printf("escolha uma das formas de pagamentos abaixo:  \n");
+printf(" 1- pagamento a vista  \n");
+printf("pagamneto a prazo  \n ");
+printf("informe a opcao desejada   \n")
+
+switch(formadepagamento) {
+
+case 1:
+
+desconto = precoproduto * 0.10;
+precofinal = precoproduto - desconto;
+
+printf("preço do produto; R$: % .2f  \n");
+printf(" forma de pagamento a vista   \n");
+printf(" valor do desconto: r$ %.2f  \n",desconto);
+printf(" total a apagar: r$: %.2f  \n", precofinal);
+break;
+
+case 2:
+do
+{
+    printf("\ndigite a quantidade de parecla:")
+    scanf("%d, &parcelas");
+
+    if (parcelas >6)
+    
+    {
+        /* code */
+    }
+    
+} while (/* condition */);
+
+}
+
+
+
+
+
+}  
+
+
+
+
