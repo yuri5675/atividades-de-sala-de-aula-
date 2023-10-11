9e2487e944b0a642205e9d590292c6df77d2dd1e
yuri5675 <144127861+yuri5675@users.noreply.github.com>
2023-10-11 08:48:45 -0300

    Add files via upload

--- menu.c
+++ menu.c
@@ -5,16 +5,51 @@ int main(){
 
 int codigo;
 
-printf(" escolha uma opcao ");
+printf("--------------MENU --------------. \n" );
+  printf ("");
+ printf( " 1 - picanha   R$: 25,00. \n");
+  printf (" 2 - lasanha   R$: 20,00. \n");
+ printf (" 3 - strogonoff R$: 18,00. \n");
+ printf (" 4- bife acebolado R$: 55,00. \n ");
+  printf ("5 - pão com ovo R$: 5,00. \n ");
+printf("");
+ printf(" -------------- FIM---------------. \n");
+
+
+
+
+
+printf(" escolha uma opcao: ");
 scanf("%d", codigo);
 
 switch (codigo){
 
-case 1:
-printf("picanha   R$: 25,00")
-break;
+    case 1:
+    printf("- picanha   R$: 25,00.");
+    break;
 
-case 2:
-printf("")
-} 
+    case 2:
+    printf("lasanha   R$: 20,00");
+    break;
+
+    case 3:
+    printf("strogonoff R$: 18,00");
+    break;
+
+    case 4:
+    printf("bife acebolado R$: 55,00");
+    break;
+
+    case 5:
+    printf(" pão com ovo R$: 5,00");
+    break;
+
+    default:
+    printf (" invalido ");
+
+
+return 0;
+}
+
+
 }
